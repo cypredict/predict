89e29cd1f5fb56061ce896d8802f8a26024fbeb3
Yang Chen <59716847+cypredict@users.noreply.github.com>
2020-01-17 15:47:31 +0800

    Add files via upload

---
+++ L2/Avazu/avazu-lgb.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+file_path = '/home/admin/avazu/train.csv'
+data = pd.read_csv(file_path,nrows = 300000)
+print(data.head())
+print(data.columns)
+print(data['click'].value_counts())
+
+data = data.drop(['id'], axis=1)
+print(pd.factorize(data['site_id'])[0])
+
+for feature in data.drop('click', axis=1).columns:
+    data[feature] = pd.factorize(data[feature])[0]
+#print(data.head())
+
+# 切分训练集，测试集
+from sklearn.model_selection import train_test_split
+train, test = train_test_split(data, test_size=0.2)
+
+# 使用LightGBM
+import lightgbm as lgb
+clf = lgb.LGBMClassifier(is_unbalanced=False, slient=False)
+cates = list(train.drop('click', axis=1).columns)
+print(cates)
+
+clf.fit(train.drop('click', axis=1), train['click'], categorical_feature=cates, verbose=5)
+predict = clf.predict_proba(test.drop('click', axis=1))[:, 1]
+
+# 定义二分类交叉熵
+def celoss(target, predict):
+    target = np.array(target)
+    predict = np.array(predict)
+    return -(target * np.log(predict) + (1 - target) * np.log(1 - predict)).mean()
+
+print(celoss(test['click'], predict))
---
+++ L2/Avazu/avazu-xgb.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+
+file_path = '/home/admin/avazu/train.csv'
+data = pd.read_csv(file_path, nrows =300000)
+print(data.head())
+print(data.columns)
+print(data['click'].value_counts())
+
+data = data.drop(['id'], axis=1)
+pd.factorize(data['site_id'])[0]
+
+for fea in data.drop('click', axis=1).columns:
+    data[fea] = pd.factorize(data[fea])[0]
+
+from sklearn.model_selection import train_test_split
+train, test = train_test_split(data, test_size=0.2)
+
+# 使用XGBoost
+import xgboost as xgb
+param = {'boosting_type':'gbdt',
+                         'objective' : 'binary:logistic',
+                         #'eval_metric' : 'auc',
+                         'eval_metric' : 'logloss',
+                         'eta' : 0.01,
+                         'max_depth' : 15,
+                         'colsample_bytree':0.8,
+                         'subsample': 0.9,
+                         'subsample_freq': 8,
+                         'alpha': 0.6,
+                         'lambda': 0,
+        }
+
+train_data = xgb.DMatrix(train.drop('click', axis=1), label=train['click'])
+test_data = xgb.DMatrix(test.drop('click', axis=1), label=test['click'])
+model = xgb.train(param, train_data, evals=[(train_data, 'train'), (test_data, 'valid')], num_boost_round = 500, early_stopping_rounds=50, verbose_eval=25)
+
+cates = list(train.drop('click', axis=1).columns)
+print('cates=', cates)
+
+predict = model.predict(test_data)
+
+# 定义二分类交叉熵
+def celoss(target, predict):
+    target = np.array(target)
+    predict = np.array(predict)
+    return -(target * np.log(predict) + (1 - target) * np.log(1 - predict)).mean()
+
+print(celoss(test['click'], predict))
